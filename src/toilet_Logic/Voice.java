b9507f5d4866c4a1675febfb43f248d02f228bbc
Wes McDorman <wmcdorma@ggc.edu>
2018-01-22 17:43:20 -0500

    Initial commit, outline of logic done

---
+++ src/toilet_Logic/Voice.java
@@ -0,0 +1,9 @@
+package toilet_Logic;
+
+public class Voice {
+	//TODO place holders for pre-recorded voice through a speaker
+	public void getAngry() {
+		System.out.println("You're making me angry!");
+	}
+
+}
