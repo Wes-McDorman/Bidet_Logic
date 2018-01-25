3b5d95b9f2d27f1bc2bf4ddea616e85c214ab293
Wes McDorman <wmcdorma@ggc.edu>
2018-01-24 22:45:59 -0500

    Small update

--- src/toilet_Logic/Main.java
+++ src/toilet_Logic/Main.java
@@ -3,6 +3,9 @@ package toilet_Logic;
 public class Main {
 	public static void main(String[] args) {
 		
+		//TODO sensor for the toilet seat to determine if it is up or down
+		
+		
 		WaterSquirt bidet = new WaterSquirt(); //instantiate WaterSquirt class
 		System.out.println("Bidet Armed");
 		bidet.isSquirting(); //Say if the water jet is off
